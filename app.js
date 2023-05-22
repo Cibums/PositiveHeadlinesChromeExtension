27bac51232b9b6aa73c45638d485c28d9f2b197f
Cibums <lucas.fransson@hotmail.com>
2023-05-22 21:56:21 +0200

    Nicer app.js

--- app.js
+++ app.js
@@ -1,117 +1,82 @@
 let previousUrl = "";
 
+// Configurations for each domain
+const domainConfig = {
+    "aftonbladet.se": {
+        getHeader: () => document.querySelector("h1"),
+        getArticleText: () => getArticleText('article', 'p, p strong, p b')
+    },
+    "dn.se": {
+        getHeader: () => document.querySelector("h1"),
+        getArticleText: () => {
+            let div1Text = getArticleText('div.article__content', 'p, p strong, p b');
+            let div2Text = getArticleText('div.article__premium-content', 'p, p strong, p b');
+            return div1Text + " " + div2Text;
+        }
+    },
+    "bbc.com": {
+        getHeader: () => document.querySelector("h1"),
+        getArticleText: () => getArticleText('article', 'p, p strong, p b')
+    },
+    "svt.se": {
+        getHeader: () => {
+            let article = document.querySelector('article.nyh_article');
+            return article.querySelector("h1");
+        },
+        getArticleText: () => getArticleText('article.nyh_article', 'p, p strong, p b')
+    },
+    "elmundo.es": {
+        getHeader: () => {
+            let article = document.querySelector('article.ue-c-article');
+            return article.querySelector("h1");
+        },
+        getArticleText: () => getArticleText('article.ue-c-article', 'p, p strong, p b')
+    }
+};
+
 function handleUrlChange() {
     const currentUrl = window.location.href;
 
     if (currentUrl !== previousUrl) {
         previousUrl = currentUrl;
 
-        let d = window.location.hostname.replace("www.", "");
+        let domain = window.location.hostname.replace("www.", "");
 
-        var header = getHeader(d);
+        let config = domainConfig[domain];
+        if(!config) return;
 
-        if(header !== null){
-            var org = header.innerHTML;
+        let header = config.getHeader();
+        if(!header) return;
 
-            var joinedText = getArticleText(d);
-            header.innerHTML = joinedText.length;
+        let org = header.innerHTML;
+        let joinedText = config.getArticleText();
 
-            if(joinedText.length < 400){
-                header.innerHTML = org;
-                return;
-            }
-
-            header.innerHTML = "Loading...";
-
-            chrome.runtime.sendMessage({contentScriptQuery: "backend", domain: d, title: org, article: joinedText}, 
-                function(response) {
-                    if(chrome.runtime.lastError) {
-                        header.innerHTML = org;
-                    } else {
-
-                        if(response.success){
-                            header.innerHTML = response.data.newTitle;
-                        }
-                        else{
-                            header.innerHTML = org;
-                        }
-                    }
-                }
-            );
+        if(joinedText.length < 400){
+            header.innerHTML = org;
+            return;
         }
+
+        header.innerHTML = "Loading...";
+
+        chrome.runtime.sendMessage({contentScriptQuery: "backend", domain: domain, title: org, article: joinedText}, 
+            function(response) {
+                if(chrome.runtime.lastError || !response.success) {
+                    header.innerHTML = org;
+                } else {
+                    header.innerHTML = response.data.newTitle;
+                }
+            }
+        );
     }
 }
 
-function getHeader(domain){
-    switch(domain){
-        case "aftonbladet.se":
-            return document.querySelector("h1");
-        case "dn.se":
-            return document.querySelector("h1");
-        case "bbc.com":
-            return document.querySelector("h1");
-        case "svt.se":
-            let article1 = document.querySelector('article.nyh_article');  // select the article tag
-            return article1.querySelector("h1");
-        case "elmundo.es":
-            let article2 = document.querySelector('article.ue-c-article');  // select the article tag
-            return article2.querySelector("h1");
-        default:
-            return null;
-    }
+function getArticleText(tag, subtags){
+    let article = document.querySelector(tag); 
+    let paragraphs = article.querySelectorAll(subtags);  
+    let textContents = Array.from(paragraphs).map(p => p.textContent); 
+    return textContents.join(' ');  
 }
 
-function getArticleText(domain){
-    if(domain === "aftonbladet.se"){
-        let article = document.querySelector('article');  // select the article tag
-        let paragraphs = article.querySelectorAll('p, p strong, p b');  // select direct child p tags
-        let textContents = Array.from(paragraphs).map(p => p.textContent);  // extract text content of each p tag
-        let joinedText = textContents.join(' ');  // join all text contents into one string
-        return joinedText;
-    }
-
-    if(domain === "bbc.com"){
-        let article = document.querySelector('article');  // select the article tag
-        let paragraphs = article.querySelectorAll('p, p strong, p b');  // select direct child p tags
-        let textContents = Array.from(paragraphs).map(p => p.textContent);  // extract text content of each p tag
-        let joinedText = textContents.join(' ');  // join all text contents into one string
-        return joinedText;
-    }
-
-    if(domain === "svt.se"){
-        let article = document.querySelector('article.nyh_article');  // select the article tag
-        let paragraphs = article.querySelectorAll('p, p strong, p b');  // select direct child p tags
-        let textContents = Array.from(paragraphs).map(p => p.textContent);  // extract text content of each p tag
-        let joinedText = textContents.join(' ');  // join all text contents into one string
-        return joinedText;
-    }
-
-    if(domain === "elmundo.es"){
-        let article = document.querySelector('article.ue-c-article');  // select the article tag
-        let paragraphs = article.querySelectorAll('p, p strong, p b');  // select direct child p tags
-        let textContents = Array.from(paragraphs).map(p => p.textContent);  // extract text content of each p tag
-        let joinedText = textContents.join(' ');  // join all text contents into one string
-        return joinedText;
-    }
-
-    if(domain === "dn.se"){
-        let div1 = document.querySelector('div.article__content');  // select the div with class1
-        let paragraphs1 = div1.querySelectorAll('p, p strong, p b');  // select all p tags under the div with class1
-
-        let div2 = document.querySelector('div.article__premium-content');  // select the div with class2
-        let paragraphs2 = div2.querySelectorAll('p, p strong, p b');  // select all p tags under the div with class2
-
-        let allParagraphs = [...paragraphs1, ...paragraphs2];  // combine the NodeList into an array
-        let textContents = Array.from(allParagraphs).map(p => p.textContent);  // extract text content of each p tag
-        let joinedText = textContents.join(' ');  // join all text contents into one string
-        return joinedText;
-    }
-
-    return "";
-}
-
-// Call handleUrlChange now to handle the page load event (first time)
 handleUrlChange();
 
-// And set up an interval to check regularly
 setInterval(handleUrlChange, 1000);  // Check every second
