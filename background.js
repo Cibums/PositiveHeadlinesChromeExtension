61e6a2997ee88586ea1bc9b6eb779e0c8cc1f43d
Cibums <lucas.fransson@hotmail.com>
2023-05-24 21:11:27 +0200

    Fetch all titles from database from start, only get from dtabase if not already fetched

--- background.js
+++ background.js
@@ -1,12 +1,36 @@
 try {
+    var baseApiUrl = "https://secure-plateau-84403.herokuapp.com/";
+    var headlines = [];
+
+    function fetchAllTitles() {
+        fetch(baseApiUrl + "title", {
+            method: "GET"
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            headlines = data;
+        })
+        .catch(error => console.error('Error:', error));
+    }
+
     chrome.runtime.onMessage.addListener(
         function(request, sender, sendResponse) {
             if (request.contentScriptQuery === "backend") {
-                var url = "https://secure-plateau-84403.herokuapp.com/title";
-
                 // Validate that we have all necessary data
                 if (!request.domain || !request.title || !request.article) {
                     sendResponse({ success: false, error: "Incomplete request data" });
+                    return;
+                }
+
+                let enc_title = btoa(encodeURIComponent(request.title));
+                let foundHeadline = headlines.find(headline => headline.enc_title === enc_title);
+                if(foundHeadline){
+                    sendResponse({ success: true, data: foundHeadline });
                     return;
                 }
 
@@ -16,7 +40,7 @@ try {
                     articleText: request.article
                 };
 
-                fetch(url, {
+                fetch(baseApiUrl + "title", {
                     method: "POST",
                     headers: {
                         'Content-Type': 'application/json'
@@ -34,7 +58,21 @@ try {
 
                 return true;  // Will respond asynchronously.
             }
-        });   
+        }
+    );
+
+    fetchAllTitles();
+    
+    // create an alarm for fetching API data every 10 minutes
+    chrome.alarms.create('fetchAllTitles', { periodInMinutes: 10 });
+
+    // listen for when this alarm fires
+    chrome.alarms.onAlarm.addListener(function(alarm) {
+        if (alarm.name === 'fetchAllTitles') {
+            fetchAllTitles();
+        }
+    });
+
 } catch(error) {
     console.error(error);
 }
